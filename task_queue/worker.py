3e1b204ef6f9fc2960521b72992c0b2cfb5a5c4c
namgivu <namgivu@gmail.com>
2017-12-06 17:01:42 +0700

    clarify code

--- task_queue/worker.py
+++ task_queue/worker.py
@@ -15,10 +15,12 @@ def callback(ch, method, properties, body):
   print(" [x] Done")
 
   #send an ack to acknowledge rabbitmq server that the task/message is done i.e. it can be deleted/no re-queue for it
-  ch.basic_ack(delivery_tag=method.delivery_tag)
+  ch.basic_ack(delivery_tag=method.delivery_tag) #TODO how to get autoAck=true
 
 
-channel.basic_qos(prefetch_count=1) #a worker only get new task only after it finishes its current one
+channel.basic_qos(prefetch_count=1) #a worker only get new task only after it finishes its current one;
+                                    #make channel to accept only consumer that has one unack-ed message at a time
+
 channel.basic_consume(callback, queue='task_queue')
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
