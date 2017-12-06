ffc2a07e2efef6fad2fecee2adfbc1482e4ddfc5
namgivu <namgivu@gmail.com>
2017-12-06 17:08:22 +0700

    TODO note

--- hello/util.sh
+++ hello/util.sh
@@ -5,6 +5,7 @@
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s" #get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 
 #download dependencies keep remote filename ref. https://stackoverflow.com/a/7451779/248616
+#TODO how to register these deps using maven?
 cd "$SCRIPT_HOME"
     curl -O http://central.maven.org/maven2/com/rabbitmq/amqp-client/4.0.2/amqp-client-4.0.2.jar
     curl -O http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.22/slf4j-simple-1.7.22.jar
